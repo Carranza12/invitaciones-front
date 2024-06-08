f4944fdf1b9e37d3c76af9f0499d75f6bd865755
carranza12 <franckgta3@gmail.com>
2024-06-08 09:34:23 -0600

    Cambios register

--- src/app/public/register.service.service.ts
+++ src/app/public/register.service.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environment';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RegisterServiceService {
   private URL_USER = environment.backendHost + '/api/auth/users';
   private URL_COMPANY = environment.backendHost + '/api/owner';
   private URL_OWNER = environment.backendHost + '/api/auth/users';
 
-  constructor() { }
+  constructor(private _httpSvc: HttpClient) {}
 
-  public createNewOwner(){
+  public createNewOwner(form: any) {
+    //crear el usuario
+    const user = {
+      email: form.email,
+      password: form.password,
+      name: form.name,
+      last_name: form.last_name,
+      role: 'owner',
+      profileImage: 'default',
+    };
+
+    const userResponse:any = this._httpSvc.post(`${this.URL_USER}`, user, {});
+
+    console.log("user response:", userResponse)
+
     
   }
 }
